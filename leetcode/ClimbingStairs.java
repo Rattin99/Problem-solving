5283a620ffe55dedebb47fede8eec4e041591346
Rattin99 <rattin99m18@gmail.com>
2021-07-10 03:48:08 +0600

    dynamic programming problems can go fuck themselves

---
+++ leetcode/ClimbingStairs.java
@@ -0,0 +1,19 @@
+// climbing stairs, problem no: 70, easy
+public class ClimbingStairs {
+    public static void main(String[] args) {
+        System.out.print(climbStairs(10));
+    }
+
+    // bottom-up dynamic programming solution
+    static public int climbStairs(int n) {
+        int[] dp = new int[n + 1];
+        dp[0] = 1;
+        dp[1] = 2;
+
+        for (int i = 3; i <= n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+
+        return dp[n];
+    }
+}
