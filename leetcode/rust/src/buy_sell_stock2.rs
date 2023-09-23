e811f9d3ddac1a49a6257d9efc2ae6029e4ae876
Rattin99 <rattin99m18@gmail.com>
2023-09-23 23:54:55 +0600

    meh

--- leetcode/rust/src/buy_sell_stock2.rs
+++ leetcode/rust/src/buy_sell_stock2.rs
@@ -20,3 +20,17 @@ pub fn max_profit(prices: Vec<i32>) -> i32 {
 
     return max_profit;
 }
+
+pub fn max_profit_simpler(prices: Vec<i32>) -> i32 {
+    let mut max_profit = 0;
+
+    for i in 1..prices.len()  {
+        let profit = prices[i] - prices[i-1];
+
+        if profit > 0 {
+            max_profit += profit;
+        }
+    }
+
+    return max_profit;
+}
--- leetcode/rust/src/main.rs
+++ leetcode/rust/src/main.rs
@@ -1,10 +1,10 @@
 mod buy_sell_stock2;
 
-use buy_sell_stock2::max_profit;
+use buy_sell_stock2::max_profit_simpler;
 
 fn main() {
    let prices = vec![1,9,6,9,1,7,1,1,5,9,9,9];
-   let answer = max_profit(prices);
+   let answer = max_profit_simpler(prices);
 
    println!("{}",answer);
 }
